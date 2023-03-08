1eab83877faa6d35f7371ea58d5b7419641f9b40
Joy-Juyoung <ljy9407@gmail.com>
2023-03-08 16:56:11 -0700

    Add login and register frame and make common elements for button and input

---
+++ src/components/InputElements.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const Input = styled.input`
+  /* width: ${({ width }) => width}; */
+  width: 100%;
+  height: 50px;
+  border-radius: 6px;
+  background: #fff;
+  color: #0a0f18;
+  font-size: 16px;
+  outline: none;
+  cursor: text;
+  border: 1px solid #a3a9b3;
+  margin: 0 0 10px;
+  padding: 0 8px;
+
+  &::placeholder {
+    color: #a3a9b3;
+  }
+
+  &:focus {
+    border: 0.1px solid #0a0f18;
+  }
+`;
+
+// 입력되면 전체삭제 기능 X 아이콘 생성
---
+++ src/pages/RegisterPage/RegisterComfirm.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { ButtonLarge } from '../../components/ButtonElements';
+import { Input } from '../../components/InputElements';
+import { PesnalContainer, PesnalWrapper } from '../StyledElements';
+
+const RegisterComfirm = () => {
+  return (
+    <PesnalContainer>
+      <PesnalWrapper>
+        Confirm Your Email Address
+        <div>We sent the verification code to your email.</div>
+        <div>
+          email@gmai.com
+          <button>Edit</button>
+          {/* Edit 누르면 이전 페이지로 돌아감 */}
+        </div>
+        <div>
+          <Input placeholder='Enter your verificaiton code.' />
+          <button>Resend</button>
+        </div>
+        <div>The verification code is vaild for 30 minutes.</div>
+        <ButtonLarge>Confirm</ButtonLarge>
+        {/* onClick -> 모달 Tanks you message & Continue shopping click -> 자동로그인 후 Home으로 */}
+      </PesnalWrapper>
+    </PesnalContainer>
+  );
+};
+
+export default RegisterComfirm;
