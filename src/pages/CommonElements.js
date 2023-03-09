ebebc5c34d8b8ca6b9ebb620023eec9cd2edcf46
Joy-Juyoung <ljy9407@gmail.com>
2023-03-09 03:24:59 -0700

    Fix Register page to change styling for the validation and change the router at app.js and Home.js to be able to see the header any pages

---
+++ src/pages/CommonElements.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+export const PesnalContainer = styled.div`
+  max-width: 1280px;
+  margin: 0 auto;
+  font-size: 14px;
+  color: #0a0f18;
+
+  @media screen and (min-width: 576px) {
+    max-width: 375px;
+  }
+`;
+
+export const PesnalWrapper = styled.div`
+  padding: 20px 20px 0;
+  margin: 0 auto;
+`;
+
+export const InputPassword = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const EyeIcon = styled.div`
+  color: #a3a9b3;
+  margin: 0 8px 10px -28px;
+  cursor: pointer;
+`;
--- src/pages/RegisterPage/RegisterComfirm.js
+++ src/pages/RegisterPage/RegisterComfirm.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { ButtonLarge } from '../../components/ButtonElements';
 import { Input } from '../../components/InputElements';
-import { PesnalContainer, PesnalWrapper } from '../StyledElements';
+import { PesnalContainer, PesnalWrapper } from '../CommonElements';
+
+// Confirm버튼은 verifcation code가 입력되면 활성화
 
 const RegisterComfirm = () => {
   return (
@@ -10,9 +12,10 @@ const RegisterComfirm = () => {
         Confirm Your Email Address
         <div>We sent the verification code to your email.</div>
         <div>
-          email@gmai.com
+          {/* 앞에서 입력한 email 자동으로 입력 */}
+          <div>email@gmai.com</div>
           <button>Edit</button>
-          {/* Edit 누르면 이전 페이지로 돌아감 */}
+          {/* Edit 누르면 이전 페이지로 돌아감. 이때 이전에 입려한 정보는 유지하기 */}
         </div>
         <div>
           <Input placeholder='Enter your verificaiton code.' />
