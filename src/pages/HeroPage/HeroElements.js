e908fcd2ddfa727b97e5a20597eb3d2f57d1f2f6
Chang Han Kim <827637@ACDM.DS.SAIT.CA>
2023-03-11 16:51:16 -0700

    Modified Main page CSS

--- src/pages/HeroPage/HeroElements.js
+++ src/pages/HeroPage/HeroElements.js
@@ -147,7 +147,246 @@ export const ParagraghBody = styled.p`
 `;
 
 export const OneRightInfoDown = styled.div`
+    margin-top: 40px;
+`;
 
+export const SectionTwo = styled.section`
+    padding-top: 80px;
+`;
+
+export const SectionTwoWrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+    -webkit-box-align: center;
+    align-items: center;
+    padding: 0px 20px;
+    max-width: 1280px;
+    margin-left: auto;
+    margin-right: auto;
+    box-sizing: border-box;
+`;
+
+export const SectionTwoLeftInfo = styled.div`
+    flex: 1 1 0%;
+    padding: 0px 50px 0px 0px;
+`;
+
+export const TwoLeftInfoUp = styled.div`
+    display: block;
+    a {
+        text-Decoration: none;
+        color: black;
+    }
+`;
+
+export const TwoLeftInfoDown = styled.div`
+    margin-top: 40px;
+`;
+
+export const ParagraghTwoTitle = styled.h2`
+    font-size: 36px;
+    line-height: 1.2;
+    font-weight: 500;
+    display: -webkit-box;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: normal;
+    overflow-wrap: break-word;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+    word-break: break-word;
+`;
+
+export const ParagraghTwoBody = styled.p`
+    margin-top: 32px;
+    font-size: 20px;
+    line-height: 1.3;
+    word-break: break-word;
+`;
+
+export const SectionTwoRightInfo = styled.div`
+    flex-shrink: 0;
+    padding-bottom: 58.96%;
+    width: calc(50% - 8px);
+    position: relative;
+    display: block;
+    background-color: rgb(255, 255, 255);
+    a {
+        text-Decoration: none;
+        color: black;
+    }
+`;
+
+export const SecondImg = styled.img`
+    position: absolute;
+    left: 0px;
+    top: 0px;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    mix-blend-mode: multiply;
+    border: none;
+    line-height: 0;
+    vertical-align: top;
 
 `;
 
+
+export const SectionThree = styled.section`
+    padding-top: 80px;
+`;
+export const SectionThreeWrapper = styled.div`
+    display: flex;
+    -webkit-box-align: center;
+    align-items: center;
+    padding: 0px 20px;
+    flex-direction: row-reverse;
+    max-width: 1280px;
+    margin-left: auto;
+    margin-right: auto;
+    box-sizing: border-box;
+`;
+
+export const SectionThreeLeftInfo = styled.div`
+    flex-shrink: 0;
+    padding-bottom: 30.888%;
+    width: calc(50% - 8px);
+    position: relative;
+    display: block;
+    background-color: rgb(255, 255, 255);
+    a {
+        text-Decoration: none;
+        color: black;
+    }
+`;
+
+export const ThirdImg = styled.img`
+    position: absolute;
+    left: 0px;
+    top: 0px;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    mix-blend-mode: multiply;
+    border: none;
+    line-height: 0;
+    vertical-align: top;
+`;
+export const SectionThreeRightInfo = styled.div`
+    flex: 1 1 0%;
+    padding: 0px 50px 0px 0px;
+    margin-left: 50px;
+`;
+
+export const ThreeRightInfoUp = styled.div`
+    display: block;
+    a {
+        text-Decoration: none;
+        color: black;
+    }
+`;
+
+export const ParagraghThreeTitle = styled.h2`
+    font-size: 36px;
+    line-height: 1.2;
+    font-weight: 600;
+    display: -webkit-box;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: normal;
+    overflow-wrap: break-word;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+    word-break: break-word;
+`;
+
+export const ParagraghThreeBody= styled.p`
+    margin-top: 32px;
+    font-size: 20px;
+    line-height: 1.3;
+    word-break: break-word;
+`;
+
+export const ThreeRightInfoDown = styled.div`
+    margin-top: 40px;
+`;
+
+export const SectionFour = styled.section`
+    padding-top: 80px;
+`;
+
+export const SectionFourWrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+    -webkit-box-align: center;
+    align-items: center;
+    padding: 0px 20px;
+    max-width: 1280px;
+    margin-left: auto;
+    margin-right: auto;
+    box-sizing: border-box;
+`;
+
+export const SectionFourLeftInfo = styled.div`
+    flex: 1 1 0%;
+    padding: 0px 50px 0px 0px;
+`;
+
+export const FourLeftInfoUp = styled.div`
+    display: block;
+    a {
+        text-Decoration: none;
+        color: black;
+    }
+`;
+
+export const FourLeftInfoDown = styled.div`
+    margin-top: 40px;
+`;
+
+export const ParagraghFourTitle = styled.h2`
+    font-size: 36px;
+    line-height: 1.2;
+    font-weight: 500;
+    display: -webkit-box;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: normal;
+    overflow-wrap: break-word;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+    word-break: break-word;
+`;
+
+export const ParagraghFourBody = styled.p`
+    margin-top: 32px;
+    font-size: 20px;
+    line-height: 1.3;
+    word-break: break-word;
+`;
+
+export const SectionFourthRightInfo = styled.div`
+    flex-shrink: 0;
+    padding-bottom: 58.96%;
+    width: calc(50% - 8px);
+    position: relative;
+    display: block;
+    background-color: rgb(255, 255, 255);
+    a {
+        text-Decoration: none;
+        color: black;
+    }
+`;
+
+export const FourthImg = styled.img`
+    position: absolute;
+    left: 0px;
+    top: 0px;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    mix-blend-mode: multiply;
+    border: none;
+    line-height: 0;
+    vertical-align: top;
+`;
