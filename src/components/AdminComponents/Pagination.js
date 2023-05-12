f0e2ad79621a4812ce577a9430c031a6aa9ff7f4
Joy-Juyoung <ljy9407@gmail.com>
2023-05-12 17:35:46 -0600

    Modified pagenation to display only 5 pages before clicking the arrow

--- src/components/AdminComponents/Pagination.js
+++ src/components/AdminComponents/Pagination.js
@@ -1,5 +1,16 @@
 import React from 'react';
-import { PageBtn, PaginationContainer } from './PaginationElements';
+import {
+  ArrowKey,
+  PageBtn,
+  PagenationIndex,
+  PagenationWrap,
+  PaginationContainer,
+} from './PaginationElements';
+import KeyboardDoubleArrowRightIcon from '@mui/icons-material/KeyboardDoubleArrowRight';
+import KeyboardDoubleArrowLeftIcon from '@mui/icons-material/KeyboardDoubleArrowLeft';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
+import { useState } from 'react';
 
 const Pagination = ({
   totalPosts,
@@ -8,6 +19,31 @@ const Pagination = ({
   currentPage,
 }) => {
   let pages = [];
+  const [slideIndex, setSlideIndex] = useState(0);
+  const [startNum, setStartNum] = useState(0);
+  const [endNum, setEndNum] = useState(5);
+
+  const handleClick = (direction) => {
+    if (direction === 'right') {
+      setCurrentPage(currentPage + 1);
+      if (currentPage === pages.length) {
+        setCurrentPage(currentPage);
+      }
+      if (currentPage >= startNum + 5) {
+        setStartNum(startNum + 5);
+        setEndNum(endNum + 5);
+      }
+    } else {
+      setCurrentPage(currentPage - 1);
+      if (currentPage === 1) {
+        setCurrentPage(currentPage);
+      }
+      if (currentPage === startNum + 1) {
+        setStartNum(startNum - 5);
+        setEndNum(endNum - 5);
+      }
+    }
+  };
 
   for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
     pages.push(i);
@@ -15,17 +51,30 @@ const Pagination = ({
 
   return (
     <PaginationContainer>
-      {pages.map((page, index) => {
-        return (
-          <PageBtn
-            key={index}
-            onClick={() => setCurrentPage(page)}
-            className={page == currentPage ? 'active' : ''}
-          >
-            {page}
-          </PageBtn>
-        );
-      })}
+      <PagenationWrap>
+        <ArrowKey direction='left' onClick={() => handleClick('left')}>
+          <KeyboardArrowLeftIcon />
+        </ArrowKey>
+        <PagenationIndex>
+          {pages?.slice(startNum, endNum).map((page, index) => {
+            return (
+              <PageBtn
+                key={index}
+                onClick={() => setCurrentPage(page)}
+                className={page === currentPage ? 'active' : ''}
+              >
+                {page}
+              </PageBtn>
+            );
+          })}
+        </PagenationIndex>
+        <ArrowKey direction='right' onClick={() => handleClick('right')}>
+          <KeyboardArrowRightIcon
+          // direction='right'
+          // onClick={() => handleClick('right')}
+          />
+        </ArrowKey>
+      </PagenationWrap>
     </PaginationContainer>
   );
 };
--- src/components/AdminComponents/PaginationElements.js
+++ src/components/AdminComponents/PaginationElements.js
@@ -5,6 +5,54 @@ export const PaginationContainer = styled.div`
   flex-wrap: wrap;
   justify-content: center;
   margin-top: 1.5rem;
+  /* width: 250px; */
+`;
+
+export const PagenationWrap = styled.div`
+  display: flex;
+  align-items: center;
+  /* width: 240px;
+  padding: 0 20px;
+  display: flex;
+  position: relative;
+  overflow: hidden; */
+`;
+
+export const ArrowKey = styled.div`
+  /* width: 50px; */
+  /* height: 50px; */
+  /* background-color: #fff7f7;
+  border-radius: 50%; */
+  /* display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: ${(props) => props.direction === 'left' && '0px'};
+  right: ${(props) => props.direction === 'right' && '0px'};
+  margin: auto;
+  /* padding: 10px; */
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+`;
+
+export const PagenationIndex = styled.div`
+  display: flex;
+  align-items: center;
+  /* transition: all 1s ease;
+  transform: translateX(${(props) => props.slideIndex * -250}px);
+  display: flex;
+
+  @keyframes scroll {
+    0% {
+      left: 0;
+    }
+    100% {
+      left: -100%;
+    }
+  } */
 `;
 
 export const PageBtn = styled.div`
